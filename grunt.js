33106df48b8ddf47f36d7e9d6d6c8773096f83cf
surtich <surtich@gmail.com>
2013-02-02 16:08:49 +0100

    First example with Iris

---
+++ grunt.js
@@ -0,0 +1,68 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: '<json:iris.json>',
+        meta: {
+            banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
+        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
+        },
+        concat: {
+            dist: {
+                src: ['<banner:meta.banner>', '<file_strip_banner:src/<%= pkg.name %>.js>'],
+                dest: 'dist/<%= pkg.name %>.js'
+            }
+        },
+        min: {
+            dist: {
+                src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
+                dest: 'dist/<%= pkg.name %>.min.js'
+            }
+        },
+        qunit: {
+            files: ['www/test/**/*.html']
+        },
+        lint: {
+            files: ['grunt.js', ' www/js/iris.js', 'www/js/init.js', 'www/js/shopping_list.js', 'www/shopping/**/*.js', 'www/test/modet_test.js']
+        },
+        watch: {
+            files: '<config:lint.files>',
+            tasks: 'lint qunit'
+        },
+        jshint: {
+            options: {
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                boss: true,
+                eqnull: true,
+                browser: true
+            },
+            globals: {
+                jQuery: true,
+                iris: true,
+                $: true,
+                model: true
+            }
+        },
+        uglify: {},
+        server: {
+            port:8080,
+            base: "./"
+        }
+    });
+
+    // Default task.    
+    grunt.registerTask('default', 'lint server watch');
+
+    grunt.registerTask('test', 'lint qunit');
+};
