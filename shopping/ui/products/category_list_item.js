d9b75d47d0f5f43abba7de30992fc0c7cdcbef90
surtich <surtich@gmail.com>
2013-09-04 03:16:42 +0200

    Reorganized files

--- shopping/ui/products/category_list_item.js
+++
@@ -1,9 +0,0 @@
-iris.ui(function(self) {	
-    
-    self.create = function() {
-        self.tmplMode(self.APPEND);
-        var category = self.setting("category");
-        self.tmpl(iris.path.ui.category_list_item.html, category);
-    };	
-    
-}, iris.path.ui.category_list_item.js);
